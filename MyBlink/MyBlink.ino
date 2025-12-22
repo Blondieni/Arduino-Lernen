2e1c162cf370d5dbb96104e7143b0693a1951fe7
Jana Wortmann <jana.wortmann@hsbi.de>
2025-12-22 14:45:50 +0100

    aufraeumen

---
+++ MyBlink/MyBlink.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(9, OUTPUT);      // Wir wecken Pin 9 auf
+}
+
+void loop() {
+  digitalWrite(9, HIGH);   // Strom AN (Licht an)
+  delay(1000);             // 1 Sekunde warten
+  digitalWrite(9, LOW);    // Strom AUS (Licht aus)
+  delay(1000);             // 1 Sekunde warten
+}
